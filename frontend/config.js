a633b250fa2edc56607ecb56fa1bd7f2a17c0faf
Claudio Vallejos <claudio.v18@hotmail.cl>
2022-04-27 02:23:17 -0400

    config for enviroment variables

--- frontend/config.js
+++
@@ -1,3 +0,0 @@
-export default {
-    APP_SERVICE_URL: import.meta.env.VITE_APP_SERVICE_URL,
-}
---
+++ frontend/src/config.js
@@ -0,0 +1,4 @@
+export default {
+    SERVICE_URL: import.meta.env.VITE_APP_SERVICE_URL,
+    API_URL: import.meta.env.VITE_APP_BACKEND_URL
+}

106b21dc6ac750fb6b8ce5a85e70c064ac20d5c5
Claudio Vallejos <claudio.v18@hotmail.cl>
2022-04-27 02:24:08 -0400

    identacion

--- frontend/src/views/GalleryView.vue
+++ frontend/src/views/GalleryView.vue
@@ -1,16 +1,15 @@
 <script setup>
-  import Card from "@/components/Card.vue";
+  import Card from "@/components/Card.vue"
 </script>
 
 <template> 
     <div class="album py-5 bg-light">
         <div class="container">
-
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            <Card/>
-            <Card/>
-            <Card/>
-        </div>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
         </div>
     </div>
 </template>

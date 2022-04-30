6aaab1c886353244485940492dd94703f81a684e
Claudio Vallejos <claudio.v18@hotmail.cl>
2022-04-29 20:22:14 -0400

    search bar component

---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<template>
+    <div class="d-flex">
+        <button class="btn btn-outline-success me-2" type="submit"></button>
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    </div>
+</template>

56b28c5531b5d3b5421a4470bb3305274506ae1c
Claudio Vallejos <claudio.v18@hotmail.cl>
2022-05-01 01:01:03 -0400

    galery listenning rick and moty api

--- frontend/src/views/GalleryView.vue
+++ frontend/src/views/GalleryView.vue
@@ -1,31 +1,37 @@
+<script setup>
+  import Card from "@/components/Card.vue"
+  import { onMounted, reactive } from 'vue'
+  import { storeToRefs  } from 'pinia'
+  import { getUser, getCharacters } from '@/api.js'
+  import { useUserStore } from '@/stores/user.js'
+  import { useRickAndMortyStore } from '@/stores/rickandmorty.js'
+
+  const userStore = useUserStore()
+  const rickAndMortyStore = useRickAndMortyStore()
+
+  onMounted(async () => {
+    const resUser = await getUser()
+    if(resUser.status == 200){
+      const user = resUser.content
+      userStore.login({username: user.name})
+    }
+    const resCharacters = await getCharacters()
+    if(resCharacters.results){
+      const characters = resCharacters.results
+      rickAndMortyStore.setState(characters)
+    }
+  })
+
+  const { characters } = storeToRefs(rickAndMortyStore)
+
+</script>
+
 <template> 
     <div class="album py-5 bg-light">
         <div class="container">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                <Card/>
-                <Card/>
-                <Card/>
+            <div class="row g-lg-5">
+                <Card v-for="char in characters" v-bind:key="char.id" :data="char" class="col"/>
             </div>
         </div>
     </div>
 </template>
-
-<script setup>
-  import Card from "@/components/Card.vue"
-  import { onMounted } from 'vue'
-  import { getUser } from '../api.js'
-  import { useUserStore } from '@/stores/user.js'
-
-  const userStore = useUserStore()
-
-  onMounted(async () => {
-    const res = await getUser();
-    const user = await res.content;
-
-    if(res.status === 200){
-      userStore.login({name: user.name})
-    }
-  })
-
-
-</script>

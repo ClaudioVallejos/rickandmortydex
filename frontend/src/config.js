b822651b17017eabe50790f3ad77f7feddcf25f5
Claudio Vallejos <claudio.v18@hotmail.cl>
2022-05-01 00:57:38 -0400

    rick and morty api to config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
 export default {
     SERVICE_URL: import.meta.env.VITE_APP_SERVICE_URL,
-    API_URL: import.meta.env.VITE_APP_BACKEND_URL
+    API_URL: import.meta.env.VITE_APP_BACKEND_URL,
+    API_URL_CHARACTERS: import.meta.env.VITE_APP_CHARACTERS_URL
 }

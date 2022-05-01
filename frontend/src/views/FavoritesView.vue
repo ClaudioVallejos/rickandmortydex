9c03b213f221cc11e3fa247b4666c1c50f16b6fb
Claudio Vallejos <claudio.v18@hotmail.cl>
2022-05-01 00:59:58 -0400

    mok favorte screen

---
+++ frontend/src/views/FavoritesView.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import Card from "@/components/Card.vue"
+  import { onMounted, reactive } from 'vue'
+  import { storeToRefs  } from 'pinia'
+  import { getUser, getCharacters } from '@/api.js'
+  import { useUserStore } from '@/stores/user.js'
+  import { useRickAndMortyStore } from '@/stores/rickandmorty.js'
+
+  const userStore = useUserStore()
+  const rickAndMortyStore = useRickAndMortyStore()
+
+  onMounted(async () => {
+    const resUser = await getUser()
+    if(resUser.status == 200){
+      const user = resUser.content
+      userStore.login({username: user.name})
+    }
+    const resCharacters = await getCharacters()
+    if(resCharacters.results){
+      const characters = resCharacters.results
+      rickAndMortyStore.setState(characters)
+    }
+  })
+
+  const { characters } = storeToRefs(rickAndMortyStore)
+
+</script>
+
+<template> 
+    <div class="album py-5 bg-light">
+        <div class="container">
+            <div class="row g-lg-5">
+                <!-- <Card v-for="char in characters" v-bind:key="char.id" :data="char" class="col"/> -->
+                <h1>aqui van los favoritos</h1>
+            </div>
+        </div>
+    </div>
+</template>

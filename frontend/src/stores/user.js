24b49bac5157f125d31df7117f70bb0c8f4beedf
Claudio Vallejos <claudio.v18@hotmail.cl>
2022-05-02 05:25:51 -0400

    user store favorites characters

--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -3,11 +3,20 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('userStore', {
   state: () => ({
     username: false,
+    favorites: {
+      ids: [],
+      collection: []
+    },
   }),
   actions: {
     login(payload) {
       const { username } = payload
       this.username = username
+    },
+    setFavorites(payload) {
+      const favoritesIds = payload.map(favorite => favorite.characterId)
+      this.favorites.ids = favoritesIds
+      this.favorites.collection = payload
     },
     logout() {
       this.username = false

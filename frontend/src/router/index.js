de30c8318fb1cabf3c9fbaaac6d07a693cf5756c
Claudio Vallejos <claudio.v18@hotmail.cl>
2022-05-02 05:26:29 -0400

    route for error 404

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import LoginView from "@/views/LoginView.vue";
 import GalleryView from "@/views/GalleryView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import FavoritesView from "@/views/FavoritesView.vue";
+import notFoundView from "@/views/notFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
       name: "favorites",
       component: FavoritesView,
     },
+    { 
+      path: '/:pathMatch(.*)',
+      name: 'notFound',
+      component: notFoundView 
+    }
   ],
 });
 

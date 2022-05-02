a031b2a20fca39e021262723e22edafc9298d21f
Claudio Vallejos <claudio.v18@hotmail.cl>
2022-05-02 05:26:42 -0400

    page and search store

--- frontend/src/stores/rickandmorty.js
+++ frontend/src/stores/rickandmorty.js
@@ -3,10 +3,20 @@ import { defineStore } from 'pinia'
 export const useRickAndMortyStore = defineStore('rickAndMortyStore', {
   state: () => ({
     characters: [],
+    search: '',
+    page: {
+      next: null,
+      prev: null
+    }
   }),
   actions: {
     setState(payload) {
-      this.characters = payload
+      this.characters = payload.results
+      this.page.next = payload.info.next
+      this.page.prev = payload.info.prev
+    },
+    setSearch(text) {
+      this.search = text
     }
   }
 })

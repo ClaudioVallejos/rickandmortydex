ba4ce0d6571862312e7442bcb8bec5ae96c8bf49
Claudio Vallejos <claudio.v18@hotmail.cl>
2022-05-02 05:25:31 -0400

    gallery filter dinamic

--- frontend/src/views/GalleryView.vue
+++ frontend/src/views/GalleryView.vue
@@ -1,37 +1,78 @@
 <script setup>
+  import "@/assets/styles.css"
   import Card from "@/components/Card.vue"
+  import Footer from "@/components/Footer.vue"
   import { onMounted, reactive } from 'vue'
   import { storeToRefs  } from 'pinia'
-  import { getUser, getCharacters } from '@/api.js'
+  import { getUser, getCharacters, saveFavorite } from '@/api.js'
   import { useUserStore } from '@/stores/user.js'
   import { useRickAndMortyStore } from '@/stores/rickandmorty.js'
 
   const userStore = useUserStore()
   const rickAndMortyStore = useRickAndMortyStore()
-
-  onMounted(async () => {
+  
+  const refreshUserData = async () => {
     const resUser = await getUser()
     if(resUser.status == 200){
       const user = resUser.content
+      userStore.setFavorites(user.favorites)
       userStore.login({username: user.name})
     }
-    const resCharacters = await getCharacters()
+  }
+
+  const refreshCharactersData = async (pageNumber = 1) => {
+    const resCharacters = await getCharacters(pageNumber)
     if(resCharacters.results){
-      const characters = resCharacters.results
-      rickAndMortyStore.setState(characters)
+      rickAndMortyStore.setState(resCharacters)
     }
+  }
+
+  onMounted(async () => {
+    await refreshCharactersData();
+    await refreshUserData();
+
   })
 
-  const { characters } = storeToRefs(rickAndMortyStore)
+  const handleFavorite = async (characterData) => {
+    if(!username) return false
+    const resFavorite = await saveFavorite(characterData)
+    if(resFavorite.status == 200){
+      await refreshUserData();
+    }
+  }
 
+  const handleNext = (e) => {
+    const pageNumber = e.split('=')[1]
+    refreshCharactersData(pageNumber)
+    window.scrollTo(0, 0);
+  }
+
+  const handlePrev = (e) => {
+    const pageNumber = e.split('=')[1]
+    refreshCharactersData(pageNumber)
+    window.scrollTo(0, 0);
+  }
+
+  const { characters, search, page } = storeToRefs(rickAndMortyStore)
+  const { favorites, username } = storeToRefs(userStore)
 </script>
 
-<template> 
-    <div class="album py-5 bg-light">
-        <div class="container">
-            <div class="row g-lg-5">
-                <Card v-for="char in characters" v-bind:key="char.id" :data="char" class="col"/>
-            </div>
+<template>
+<div>
+    <div class="album py-5 mt-5">
+      <div class="container">
+        <div class="row g-lg-5">
+          <Card
+            @clickFavorite="handleFavorite"
+              v-for="char in characters.filter( char => 
+                char.name.toLowerCase().includes( search.toLowerCase() ) 
+                || char.gender.toLowerCase() == ( search.toLowerCase() )
+                || char.status.toLowerCase().includes( search.toLowerCase() )  
+              )" 
+            v-bind:key="char.id" :data="char" class="col"/>
         </div>
+      </div>
     </div>
+    <Footer :pagination="true" :pages="page" @prevPage="handlePrev" @nextPage="handleNext"/>
+</div>
 </template>
